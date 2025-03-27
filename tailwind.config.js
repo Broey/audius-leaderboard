49dfcd0c64dd2bf33d94829b2793d6b8d5782b18
Broey <89108372+Broey@users.noreply.github.com>
2025-03-27 02:22:17 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,17 +10,3 @@ module.exports = {
   },
   plugins: [],
 };
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}", // if you're using App Router
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
->>>>>>> 3a6bcb3c1420f6fbdbee0d05576d5080291177cf
